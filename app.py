92654869e738defc8cb6a326f00ee8656766173b
CupOfCoffe <edwinjsa2021@gmail.com>
2024-11-17 23:02:32 -0600

    avances

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import json
+import traceback
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+import pyodbc
 
 app = Flask(__name__)
+
+db = sessionmaker(bind=create_engine('sqlite:///PulperiaElPilar.db'))()
 
 @app.route('/')
 def index():
@@ -21,6 +28,22 @@ def contado():
 @app.route('/abonarCredito')
 def abonarCredito():
     return render_template('abonarCredito.html')
+
+@app.route('/guardar-producto', methods=['POST'])
+def guardar_producto():
+    try:
+        data = json.loads(request.data)
+        name = data.get('name')
+        quantity = data.get('quantity', 0)
+        price = data.get('price', 0.0)
+        
+        #AQUI VA LA CONSULTA DE LA BASE DE DATOS
+        
+        
+        return jsonify({'name': name, 'quantity': quantity, 'price': price}), 200
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,3 +1,6 @@
+let datos = [];
+
+
 document.addEventListener("DOMContentLoaded", function(){
     // make it as accordion for smaller screens
     if (window.innerWidth < 992) {
@@ -49,3 +52,48 @@ document.addEventListener("DOMContentLoaded", function(){
       });
     });
     // DOMContentLoaded  end
+
+    window.onload = () => {
+      function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length == 2) return parts.pop().split(';').shift();
+      }
+
+      const agregar = document.getElementById("agregar");
+      agregar.addEventListener("click", () => {
+        const f = document.getElementById("product-form");
+        
+        fetch('/guardar-producto', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+          },
+          body: JSON.stringify({
+            'name': f['product-name'].value.trim(),
+            'quantity': f['product-quantity'].value,
+            'price': f['product-price'].value
+          })
+        })
+          .then(response => {
+            if (!response.ok) throw new Error('Error en la solicitud');
+            return response.json();
+          })
+          .then(data => {
+            console.log("Datos recibidos:", data);
+            const table = document.getElementById("product-table-body");
+            table.innerHTML += `
+              <tr>
+                  <td>${data.name}</td>
+                  <td>${data.quantity}</td>
+                  <td>${data.price}</td>
+                  <td>${data.quantity * data.price}</td>
+              </tr>
+            `;
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+      });
+};
