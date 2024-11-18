e1b5b74fe378b64a0817cea3a5e59c7e382f5648
svrenz <daniavaleska26@gmail.com>
2024-11-18 10:32:26 -0600

    Mensaje de commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, jsonify, request
+import json
+import traceback
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+import pyodbc
+
+app = Flask(__name__)
+
+db = sessionmaker(bind=create_engine('sqlite:///PulperiaElPilar.db'))()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/credito')
+def credito():
+    return render_template('credito.html')
+
+@app.route('/contado')
+def contado():
+    return render_template('contado.html')
+
+@app.route('/abonarCredito')
+def abonarCredito():
+    return render_template('abonarCredito.html')
+
+@app.route('/verCredito')
+def verCredito():
+    return render_template('verCredito.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+@app.route('/guardar-producto', methods=['POST'])
+def guardar_producto():
+    try:
+        data = json.loads(request.data)
+        name = data.get('name')
+        quantity = data.get('quantity', 0)
+        price = data.get('price', 0.0)
+        
+        #AQUI VA LA CONSULTA DE LA BASE DE DATOS
+        
+        
+        return jsonify({'name': name, 'quantity': quantity, 'price': price}), 200
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,99 @@
+let datos = [];
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    // make it as accordion for smaller screens
+    if (window.innerWidth < 992) {
+    
+      // close all inner dropdowns when parent is closed
+      document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+        everydropdown.addEventListener('hidden.bs.dropdown', function () {
+          // after dropdown is hidden, then find all submenus
+            this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+              // hide every submenu as well
+              everysubmenu.style.display = 'none';
+            });
+        })
+      });
+    
+      document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+        element.addEventListener('click', function (e) {
+            let nextEl = this.nextElementSibling;
+            if(nextEl && nextEl.classList.contains('submenu')) {	
+              // prevent opening link if link needs to open dropdown
+              e.preventDefault();
+              if(nextEl.style.display == 'block'){
+                nextEl.style.display = 'none';
+              } else {
+                nextEl.style.display = 'block';
+              }
+    
+            }
+        });
+      })
+    }
+    // end if innerWidth
+    }); 
+    document.addEventListener("DOMContentLoaded", function() {
+      // Despliegue del submenú al pasar el mouse
+      document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown) {
+        everydropdown.addEventListener('mouseover', function(e) {
+          let el_link = this.querySelector('.dropdown-toggle');
+          let el_menu = this.querySelector('.dropdown-menu');
+          el_menu.classList.add('show');
+          el_link.setAttribute('aria-expanded', 'true');
+        });
+        everydropdown.addEventListener('mouseleave', function(e) {
+          let el_link = this.querySelector('.dropdown-toggle');
+          let el_menu = this.querySelector('.dropdown-menu');
+          el_menu.classList.remove('show');
+          el_link.setAttribute('aria-expanded', 'false');
+        });
+      });
+    });
+    // DOMContentLoaded  end
+
+    window.onload = () => {
+      function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length == 2) return parts.pop().split(';').shift();
+      }
+
+      const agregar = document.getElementById("agregar");
+      agregar.addEventListener("click", () => {
+        const f = document.getElementById("product-form");
+        
+        fetch('/guardar-producto', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+          },
+          body: JSON.stringify({
+            'name': f['product-name'].value.trim(),
+            'quantity': f['product-quantity'].value,
+            'price': f['product-price'].value
+          })
+        })
+          .then(response => {
+            if (!response.ok) throw new Error('Error en la solicitud');
+            return response.json();
+          })
+          .then(data => {
+            console.log("Datos recibidos:", data);
+            const table = document.getElementById("product-table-body");
+            table.innerHTML += `
+              <tr>
+                  <td>${data.name}</td>
+                  <td>${data.quantity}</td>
+                  <td>${data.price}</td>
+                  <td>${data.quantity * data.price}</td>
+              </tr>
+            `;
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+      });
+};
